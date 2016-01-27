005472ad032e0c85afc62467fa7209e993b5317e
Amin Mesbah <mesbah.amin@gmail.com>
2016-01-26 21:06:53 -0800

    split a word into lists of single chars and pairs

--- speller.py
+++ speller.py
@@ -1,18 +1,38 @@
+from collections import namedtuple
 import csv
+
 
 def main():
     symbols = get_csv_data('elements.csv', 1)
     test_word = "Osiris"
-    print(find_matches(test_word, symbols))
 
-def find_matches(word, symbols):
+    tokens = tokenize_sequence(test_word)
+
+    print(tokens.singles)
+    print(tokens.pairs)
+    print(find_matches(tokens.singles, symbols))
+    print(find_matches(tokens.pairs, symbols))
+
+def tokenize_sequence(sequence):
+    """Splits a sequence into one list of individual elements, and one of pairs."""
+
+    t = namedtuple('Tokens', (['singles', 'pairs']))
+
+    singles = [sequence[i:i+1] for i in range(0, len(sequence))]
+    pairs = [sequence[i:i+2] for i in range(0, len(sequence) - 1)]
+    tokens = t(singles, pairs)
+
+    return tokens
+
+
+def find_matches(sequence, symbols):
     matches = []
 
-    for char in word:
-        single = char
-        matches += (x for x in symbols if x == char)
+    for i in sequence:
+        matches += (x for x in symbols if x == i)
 
     return matches
+
 
 def get_csv_data(file_name, column):
     symbols = []
@@ -24,6 +44,7 @@ def get_csv_data(file_name, column):
             symbols.append(row[column])
 
     return symbols
+
 
 if __name__ == '__main__':
     main()
