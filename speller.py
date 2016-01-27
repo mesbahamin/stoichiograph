6fa896ce9ceac64db5b69ec1afef8a2ee3cc9bd8
Amin Mesbah <mesbah.amin@gmail.com>
2016-01-26 16:53:49 -0800

    import list of symbols from .csv

--- speller.py
+++ speller.py
@@ -1,1 +1,20 @@
+import csv
 
+def main():
+    symbols = get_symbols('elements.csv')
+    print(symbols)
+
+
+def get_symbols(file_name):
+    symbols = []
+
+    with open(file_name) as infile:
+        csv_reader = csv.reader(infile, skipinitialspace=True, delimiter=',')
+        next(csv_reader, None)
+        for row in csv_reader:
+            symbols.append(row[1])
+
+    return symbols
+
+if __name__ == '__main__':
+    main()
